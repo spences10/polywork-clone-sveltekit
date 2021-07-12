fdfdd46b52bf4865c6f78af05a05daac7a8cb941
spences10 <spences10apps@gmail.com>
2021-07-12 19:09:20 +0100

    style: :lipstick: add daisy ui card and badges

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
-  import '../app.postcss'
+	import '../app.postcss';
 </script>
 
-<slot />
+<main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+	<div class="max-w-3xl mx-auto">
+		<slot />
+	</div>
+</main>
