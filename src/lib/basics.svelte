1d3f759de99f1e6c6749448d1847024afebfe386
spences10 <spences10apps@gmail.com>
2021-07-19 18:09:52 +0100

    chore: :recycle: move basics out to own component

---
+++ src/lib/basics.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let avatar
+  export let name
+  export let bio
+  export let username
+  export let jobTitle
+  export let company
+  export let pronoun
+  export let location
+  export let userBadges
+</script>
+
+<section class="bg-white border my-16 card shadow">
+  <div class="px-10 pt-10">
+    <div class="avatar">
+      <div class="rounded-full h-32 mb-8 w-32">
+        <img alt={avatar.altText} src={avatar.url} />
+      </div>
+    </div>
+    <h2 class="font-bold text-left leading-relaxed text-2xl">
+      {name}
+    </h2>
+    <ul class="flex space-x-2 text-left leading-relaxed opacity-75">
+      <li>@{username}</li>
+      <span class="opacity-75">&bull;</span>
+      <li>{jobTitle},</li>
+      <li>{company}</li>
+      <span class="opacity-75">&bull;</span>
+      <li>{pronoun}</li>
+      <span class="opacity-75">&bull;</span>
+      <li>{location}</li>
+    </ul>
+    <div>
+      {#if userBadges}
+        <div class="flex flex-wrap mt-5 break-words relative">
+          {#each userBadges as { name }}
+            <div
+              class="border rounded-full font-medium mr-2 mb-2 py-2 px-4"
+            >
+              {name}
+            </div>
+          {/each}
+        </div>
+      {/if}
+    </div>
+    <div class="my-5 opacity-75">
+      <p>
+        {@html bio.split('\n').join('<br />')}
+      </p>
+    </div>
+  </div>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script context="module">
+  import Basics from '$lib/basics.svelte'
   import { format } from 'date-fns'
   import { gql, GraphQLClient } from 'graphql-request'
 
@@ -76,53 +77,22 @@
 <img alt={banner.altText} src={banner.url} class="h-96 w-full" />
 
 <!-- Containers -->
-<div class="mx-auto -mt-72 max-w-7xl px-4 relative sm:px-6 lg:px-8">
+<article
+  class="mx-auto -mt-72 max-w-7xl px-4 relative sm:px-6 lg:px-8"
+>
   <div class="mx-auto max-w-3xl">
     <!-- Basics Card -->
-    <div class="bg-white border my-16 card shadow">
-      <div class="">
-        <figure class="px-10 pt-10">
-          <div class="avatar">
-            <div class="rounded-full h-32 mb-8 w-32">
-              <img alt={avatar.altText} src={avatar.url} />
-            </div>
-          </div>
-          <h2 class="font-bold text-left leading-relaxed text-2xl">
-            {name}
-          </h2>
-          <ul
-            class="flex space-x-2 text-left leading-relaxed opacity-75"
-          >
-            <li>@{username}</li>
-            <span class="opacity-75">&bull;</span>
-            <li>{jobTitle},</li>
-            <li>{company}</li>
-            <span class="opacity-75">&bull;</span>
-            <li>{pronoun}</li>
-            <span class="opacity-75">&bull;</span>
-            <li>{location}</li>
-          </ul>
-          <div>
-            {#if userBadges}
-              <div class="flex flex-wrap mt-5 break-words relative">
-                {#each userBadges as { name }}
-                  <div
-                    class="border rounded-full font-medium mr-2 mb-2 py-2 px-4"
-                  >
-                    {name}
-                  </div>
-                {/each}
-              </div>
-            {/if}
-          </div>
-          <div class="my-5 opacity-75">
-            <p>
-              {@html bio.split('\n').join('<br />')}
-            </p>
-          </div>
-        </figure>
-      </div>
-    </div>
+    <Basics
+      {avatar}
+      {name}
+      {bio}
+      {username}
+      {jobTitle}
+      {company}
+      {pronoun}
+      {location}
+      {userBadges}
+    />
 
     <div class="divider opacity-10 mb-10" />
 
@@ -155,4 +125,4 @@
       {/each}
     {/if}
   </div>
-</div>
+</article>
